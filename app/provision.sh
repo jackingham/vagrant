2769267fae67051dbd06fece96ef46163adc4c69
jackingham <jackingham1998@gmail.com>
2021-06-10 20:08:04 +0100

    added reverse proxy to app

--- app/provision.sh
+++ app/provision.sh
@@ -1,23 +1,26 @@
+#!/bin/bash
 sudo apt-get update -y
 
 sudo apt-get upgrade -y
 
-sudo apt-get install nginx -y
+sudo  apt-get install nginx -y
 
-sudo systemctl start nginx
+sudo systemctl enable nginx
 
-sudo apt-get install python-software-properties -y
+sudo apt-get install nodejs -y
 
-sudo apt-get install npm -y
+sudo apt-get install npm-y
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install python-software-properties
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
 
 sudo apt-get install nodejs -y
 
 sudo npm install pm2 -g
 
-cd app
-
 npm install
+
+cd app
 
 node app.js
